7f46911470e82ef995db02382325b0ea595dbefb
Marcial Rivera <marcial.rivera@autozone.com>
2020-09-03 19:12:26 -0500

    innital commit

---
+++ SeleniumCoolTesters/src/main/java/SeleniumCoolTesters/SeleniumCoolTesters/App.java
@@ -0,0 +1,22 @@
+package SeleniumCoolTesters.SeleniumCoolTesters;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "driver" + File.separator;
+    	System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe");
+    	WebDriver driver = new ChromeDriver();
+    	driver.get("https://www.cooltesters.com/tienda-de-cursos/selenium-con-java");
+    }
+}
